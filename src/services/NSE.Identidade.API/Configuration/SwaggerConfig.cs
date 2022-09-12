f6d396a2299cccee31ab0d800dcdf931f7dee876
Bryan <bryanlima200@outlook.com>
2022-09-11 23:24:14 -0300

    Refatorado projeto NSE.Identidade.API

---
+++ src/services/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
+                    Contact = new OpenApiContact() { Name = "Bryan Lima", Email = "contact@lima.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static WebApplication UseSwaggerConfiguration(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
