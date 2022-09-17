603c67b2be8e2b61a365d78eefce4c6e7939e020
Bryan <bryanlima200@outlook.com>
2022-09-17 00:19:43 -0300

    Implementado lógica para obter url base da API Identidade via appsettings

--- src/web/NSE.WebApp.MVC/Program.cs
+++ src/web/NSE.WebApp.MVC/Program.cs
@@ -2,8 +2,18 @@ using NSE.WebApp.MVC.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 builder.Services.AddIdentityConfiguration();
-builder.Services.AddMvcConfiguration();
+builder.Services.AddMvcConfiguration(builder.Configuration);
 builder.Services.RegisterServices();
 
 var app = builder.Build();
--- src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,21 +1,26 @@
-﻿using NSE.WebApp.MVC.Models;
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Services
 {
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
+        private readonly AppSettings _settings;
 
-        public AutenticacaoService(HttpClient httpClient)
+        public AutenticacaoService(HttpClient httpClient, 
+                                   IOptions<AppSettings> settings)
         {
             _httpClient = httpClient;
+            _settings = settings.Value;
         }
 
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
             var loginContent = ObterConteudo(usuarioLogin);
 
-            var response = await _httpClient.PostAsync("https://localhost:44301/api/identidade/autenticar", loginContent);
+            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/autenticar", loginContent);
 
             if (!TratarErrosResponse(response))
             {
@@ -32,7 +37,7 @@ namespace NSE.WebApp.MVC.Services
         {
             var registroContent = ObterConteudo(usuarioRegistro);
 
-            var response = await _httpClient.PostAsync("https://localhost:44301/api/identidade/nova-conta", registroContent);
+            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/nova-conta", registroContent);
 
             if (!TratarErrosResponse(response))
             {
