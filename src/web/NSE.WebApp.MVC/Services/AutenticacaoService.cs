25b1e4879405a639e208b752f32a1a5c0268556d
Bryan <bryanlima200@outlook.com>
2022-09-17 00:24:13 -0300

    Refatorado uso de url base da API Identidade dentro de AutenticacaoService

--- src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -7,20 +7,20 @@ namespace NSE.WebApp.MVC.Services
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
-        private readonly AppSettings _settings;
 
         public AutenticacaoService(HttpClient httpClient, 
                                    IOptions<AppSettings> settings)
         {
+            httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
+
             _httpClient = httpClient;
-            _settings = settings.Value;
         }
 
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
             var loginContent = ObterConteudo(usuarioLogin);
 
-            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/autenticar", loginContent);
+            var response = await _httpClient.PostAsync("/api/identidade/autenticar", loginContent);
 
             if (!TratarErrosResponse(response))
             {
@@ -37,7 +37,7 @@ namespace NSE.WebApp.MVC.Services
         {
             var registroContent = ObterConteudo(usuarioRegistro);
 
-            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/nova-conta", registroContent);
+            var response = await _httpClient.PostAsync("/api/identidade/nova-conta", registroContent);
 
             if (!TratarErrosResponse(response))
             {
