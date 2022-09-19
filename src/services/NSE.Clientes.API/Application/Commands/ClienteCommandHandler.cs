ba9264f5f90bc087e0cf4893ec0f1c5100b6d239
Bryan <bryanlima200@outlook.com>
2022-09-19 00:16:09 -0300

    Iniciado implementação de Command e CommandHandler

---
+++ src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        // Exemplo
+        public void Manipular(RegistrarClienteCommand command)
+        {
+            // Validar command
+
+            // Persistir na base de dados
+        }
+    }
+}
