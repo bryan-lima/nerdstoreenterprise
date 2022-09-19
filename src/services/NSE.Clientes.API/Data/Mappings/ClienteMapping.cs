a73927dbaf6f6b0bcf8bd2a839a66a57a0b60fa5
Bryan <bryanlima200@outlook.com>
2022-09-18 23:48:21 -0300

    Implementado configurações básicas da camada Data e segregado responsabilidades da classe Program (projeto NSE.Clientes.API)

---
+++ src/services/NSE.Clientes.API/Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Clientes.API.Models;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Clientes.API.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Numero)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            }).Navigation(c => c.Cpf)
+              .IsRequired();
+
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Endereco)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.EnderecoMaxLength})");
+            }).Navigation(c => c.Email)
+              .IsRequired();
+
+            // 1 : 1 => Cliente : Endereco
+            builder.HasOne(c => c.Endereco)
+                .WithOne(c => c.Cliente);
+
+            builder.ToTable("Clientes");
+        }
+    }
+}
