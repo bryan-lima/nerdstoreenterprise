56f42492970f79f7c544a6095c4df07961b86b00
Bryan <bryanlima200@outlook.com>
2022-09-27 00:19:56 -0300

    Teste do fluxo de persistência de dados e lançamento de eventos da API Clientes

--- src/services/NSE.Clientes.API/Controllers/ClientesController.cs
+++ src/services/NSE.Clientes.API/Controllers/ClientesController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
+using NSE.WebAPI.Core.Controllers;
 
 namespace NSE.Clientes.API.Controllers
 {
-    public class ClientesController : Controller
+    public class ClientesController : MainController
     {
-        public IActionResult Index()
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public ClientesController(IMediatorHandler mediatorHandler)
         {
-            return View();
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpGet("clientes")]
+        public async Task<IActionResult> Index()
+        {
+            var resultado = await _mediatorHandler.EnviarComando(new RegistrarClienteCommand(Guid.NewGuid(), "Bryan", "bryan@lima.com", "19493207072"));
+
+            return CustomResponse(resultado);
         }
     }
 }
