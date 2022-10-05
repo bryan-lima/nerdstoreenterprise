e1dcc2a502f08209117944c6a30b78ae1569f265
Bryan <bryanlima200@outlook.com>
2022-10-05 20:24:55 -0300

    Implementado as funcionalidades de atualização e remoção de itens do carrinho

--- src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -54,5 +54,33 @@
             Itens.Add(item);
             CalcularValorCarrinho();
         }
+
+        internal void AtualizarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido())
+                return;
+
+            item.AssociarCarrinho(Id);
+
+            var itemExistente = ObterPorProdutoId(item.ProdutoId);
+
+            Itens.Remove(itemExistente);
+            Itens.Add(item);
+
+            CalcularValorCarrinho();
+        }
+
+        internal void AtualizarUnidades(CarrinhoItem item, int unidades)
+        {
+            item.AtualizarUnidades(unidades);
+            AtualizarItem(item);
+        }
+
+        internal void RemoverItem(CarrinhoItem item)
+        {
+            Itens.Remove(ObterPorProdutoId(item.ProdutoId));
+
+            CalcularValorCarrinho();
+        }
     }
 }
