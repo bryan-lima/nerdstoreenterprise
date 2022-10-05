45e7cc6687eb294c94d6ad072968b313bc9e2b29
Bryan <bryanlima200@outlook.com>
2022-10-05 00:23:56 -0300

    Implementado lógica inicial para adicionar item ao carrinho

--- src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -1,4 +1,6 @@
-﻿namespace NSE.Carrinho.API.Model
+﻿using FluentValidation;
+
+namespace NSE.Carrinho.API.Model
 {
     public class CarrinhoItem
     {
@@ -16,5 +18,51 @@
         public Guid CarrinhoId { get; set; }
 
         public CarrinhoCliente CarrinhoCliente { get; set; }
+
+        internal void AssociarCarrinho(Guid carrinhoId)
+        {
+            CarrinhoId = carrinhoId;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * Valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal bool EhValido()
+        {
+            return new ItemPedidoValidation().Validate(this).IsValid;
+        }
+
+        public class ItemPedidoValidation : AbstractValidator<CarrinhoItem>
+        {
+            public ItemPedidoValidation()
+            {
+                RuleFor(c => c.ProdutoId)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do produto inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do produto não foi informado");
+
+                RuleFor(c => c.Quantidade)
+                    .GreaterThan(0)
+                    .WithMessage("A quantidade mínima de um item é 1");
+
+                RuleFor(c => c.Quantidade)
+                    .LessThan(CarrinhoCliente.MAX_QUANTIDADE_ITEM)
+                    .WithMessage($"A quantidade máxima de um item é {CarrinhoCliente.MAX_QUANTIDADE_ITEM}");
+
+                RuleFor(c => c.Valor)
+                    .GreaterThan(0)
+                    .WithMessage("O valor do item precisa ser maior que 0");
+            }
+        }
     }
 }
