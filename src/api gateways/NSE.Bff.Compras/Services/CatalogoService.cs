bbf492b85fe0d0ca70677abf562a5c16eff26fa0
Bryan <bryanlima200@outlook.com>
2022-10-09 23:23:18 -0300

    Configurações iniciais da controller Carrinho do BFF Compras, e adicionado resolução de injeção de dependências

--- src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.Bff.Compras.Extensions;
+using NSE.Bff.Compras.Models;
 
 namespace NSE.Bff.Compras.Services
 {
     public interface ICatalogoService
     {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
     }
 
     public class CatalogoService : Service, ICatalogoService
@@ -15,6 +17,15 @@ namespace NSE.Bff.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+        }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
         }
     }
 }
--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -42,7 +42,7 @@ namespace NSE.WebApp.MVC.Configuration
 
     #region PollyExtension
 
-    public class PollyExtensions
+    public static class PollyExtensions
     {
         public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
         {
