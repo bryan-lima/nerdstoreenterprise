8fdbbc88ac3bbc2eb0714a43e9f9395f700b30a0
Bryan <bryanlima200@outlook.com>
2022-10-10 23:40:23 -0300

    Adicionado camadas Domain e Infra na API Pedido e realizado configurações iniciais básicas

---
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using NSE.Core.Mediator;
+using NSE.Pedidos.Domain.Vouchers;
+using NSE.Pedidos.Infra.Data.Repository;
+using NSE.Pedidos.Infra.Data;
+using NSE.WebAPI.Core.Usuario;
+
+namespace NSE.Pedido.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<PedidosContext>();
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+        }
+    }
+}
---
+++ src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
@@ -0,0 +1,14 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Domain.Vouchers
+{
+    public class Voucher : Entity, IAggregateRoot
+    {
+
+    }
+}
