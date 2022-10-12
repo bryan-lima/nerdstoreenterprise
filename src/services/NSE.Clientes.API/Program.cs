b766aa5f341fd84d3406166b3a124a8e27ecc7cc
Bryan <bryanlima200@outlook.com>
2022-10-11 23:41:28 -0300

    Corrigido mapeamento de appsettings no Program da API Clientes

--- src/services/NSE.Clientes.API/Program.cs
+++ src/services/NSE.Clientes.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("appsettings.json", true, true)
-    .AddJsonFile($"appsettings{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
     .AddEnvironmentVariables();
 
 if (builder.Environment.IsDevelopment())
