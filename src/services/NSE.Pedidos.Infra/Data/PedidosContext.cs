9da8d408facfcadd1f55f6414e57b8bca7fbf684
Bryan <bryanlima200@outlook.com>
2022-10-12 00:40:56 -0300

    Adicionado propriedades na entidade Voucher e configurado mapeamento

--- src/services/NSE.Pedidos.Infra/Data/PedidosContext.cs
+++ src/services/NSE.Pedidos.Infra/Data/PedidosContext.cs
@@ -4,6 +4,7 @@ using NSE.Core.Data;
 using NSE.Core.DomainObjects;
 using NSE.Core.Mediator;
 using NSE.Core.Messages;
+using NSE.Pedidos.Domain.Vouchers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,8 @@ namespace NSE.Pedidos.Infra.Data
         {
             _mediatorHandler = mediatorHandler;
         }
+
+        public DbSet<Voucher> Vouchers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
