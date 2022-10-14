5812869ac5491cbd14034e7ea903bafbd18d54a9
Bryan <bryanlima200@outlook.com>
2022-10-13 23:26:06 -0300

    Adicionado querie e implementado Specification Pattern

--- src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
+++ src/services/NSE.Pedidos.Domain/Vouchers/Voucher.cs
@@ -1,4 +1,5 @@
 ﻿using NSE.Core.DomainObjects;
+using NSE.Pedidos.Domain.Vouchers.Specs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,5 +20,20 @@ namespace NSE.Pedidos.Domain.Vouchers
         public DateTime DataValidade { get; private set; }
         public bool Ativo { get; private set; }
         public bool Utilizado { get; private set; }
+
+        public bool EstaValidoParaUtilizacao()
+        {
+            return new VoucherAtivoSpecification()
+                .And(new VoucherDataSpecification())
+                .And(new VoucherQuantidadeSpecification())
+                .IsSatisfiedBy(this);
+        }
+
+        public void MarcarComoUtilizado()
+        {
+            Ativo = false;
+            Utilizado = true;
+            Quantidade = 0;
+        }
     }
 }
