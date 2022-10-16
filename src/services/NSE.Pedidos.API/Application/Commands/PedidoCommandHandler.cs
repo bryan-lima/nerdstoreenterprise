dc5cab3a5c2c393e2d3e00f0ae1d783a3d2b3eeb
Bryan <bryanlima200@outlook.com>
2022-10-16 17:04:59 -0300

    Iniciado implementação da camada application de Pedido

---
+++ src/services/NSE.Pedidos.API/Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Core.Messages;
+using NSE.Pedidos.API.Application.DTO;
+
+namespace NSE.Pedidos.API.Application.Commands
+{
+    public class PedidoCommandHandler : CommandHandler, IRequestHandler<AdicionarPedidoCommand, ValidationResult>
+    {
+        public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+        {
+            // Validação do comando
+
+            // Mapear Pedido
+
+            // Aplicar voucher, se houver
+
+            // Validar pedido
+
+            // Processar pagamento
+
+            // Se pagamento tudo OK!
+
+            // Adicionar Evento
+
+            // Adicionar Pedido Repositório
+
+            // Persistir dados de pedido e voucher
+
+            throw new NotImplementedException();
+        }
+    }
+}
