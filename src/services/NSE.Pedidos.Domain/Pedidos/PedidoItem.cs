db6398b1bbaa84dcc80f16a01183ff366af5aa08
Bryan <bryanlima200@outlook.com>
2022-10-16 14:18:36 -0300

    Realizado modelagem do pedido no domínio

---
+++ src/services/NSE.Pedidos.Domain/Pedidos/PedidoItem.cs
@@ -0,0 +1,42 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Domain.Pedidos
+{
+    public class PedidoItem : Entity
+    {
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario, string produtoImagem = null)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+            ProdutoImagem = produtoImagem;
+        }
+
+        // EF ctor
+        protected PedidoItem()
+        {
+
+        }
+
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public string ProdutoImagem { get; set; }
+
+        // EF Rel.
+        public Pedido Pedido { get; set; }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+    }
+}
