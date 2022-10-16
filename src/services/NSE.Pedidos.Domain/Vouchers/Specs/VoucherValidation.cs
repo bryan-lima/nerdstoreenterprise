09229653fa04d5f30f3cca2663a2d11762d496fe
Bryan <bryanlima200@outlook.com>
2022-10-16 17:39:51 -0300

    Implementado camada application de Pedido

---
+++ src/services/NSE.Pedidos.Domain/Vouchers/Specs/VoucherValidation.cs
@@ -0,0 +1,23 @@
+﻿using NetDevPack.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Domain.Vouchers.Specs
+{
+    public class VoucherValidation : SpecValidator<Voucher>
+    {
+        public VoucherValidation()
+        {
+            var dataSpec = new VoucherDataSpecification();
+            var qdeSpec = new VoucherQuantidadeSpecification();
+            var ativoSpec = new VoucherAtivoSpecification();
+
+            Add("dataSpec", new Rule<Voucher>(dataSpec, "Este voucher está expirado"));
+            Add("qdeSpec", new Rule<Voucher>(qdeSpec, "Este voucher já foi utilizado"));
+            Add("ativoSpec", new Rule<Voucher>(ativoSpec, "Este voucher não está mais ativo"));
+        }
+    }
+}
