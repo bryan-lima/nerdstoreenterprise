685e4b4b3f862034a448a6baff75f6092fe4a37a
Bryan <bryanlima200@outlook.com>
2022-10-16 18:08:44 -0300

    Adicionado exemplo de dapper multi mapping (não funcional para esse cenário, apenas exemplo)

--- src/services/NSE.Pedidos.API/Application/Queries/PedidoQueries.cs
+++ src/services/NSE.Pedidos.API/Application/Queries/PedidoQueries.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Pedidos.API.Application.DTO;
+﻿using Dapper;
+using NSE.Pedidos.API.Application.DTO;
 using NSE.Pedidos.Domain.Pedidos;
 
 namespace NSE.Pedidos.API.Application.Queries
@@ -20,6 +21,25 @@ namespace NSE.Pedidos.API.Application.Queries
 
         public async Task<PedidoDTO> ObterUltimoPedido(Guid clienteId)
         {
+            const string sql = @"SELECT
+                                P.ID AS 'ProdutoId', P.CODIGO, P.VOUCHERUTILIZADO, P.DESCONTO, P.VALORTOTAL,P.PEDIDOSTATUS,
+                                P.LOGRADOURO,P.NUMERO, P.BAIRRO, P.CEP, P.COMPLEMENTO, P.CIDADE, P.ESTADO,
+                                PIT.ID AS 'ProdutoItemId',PIT.PRODUTONOME, PIT.QUANTIDADE, PIT.PRODUTOIMAGEM, PIT.VALORUNITARIO 
+                                FROM PEDIDOS P 
+                                INNER JOIN PEDIDOITEMS PIT ON P.ID = PIT.PEDIDOID 
+                                WHERE P.CLIENTEID = @clienteId 
+                                AND P.DATACADASTRO between DATEADD(minute, -3,  GETDATE()) and DATEADD(minute, 0,  GETDATE())
+                                AND P.PEDIDOSTATUS = 1 
+                                ORDER BY P.DATACADASTRO DESC";
+
+            var pedido = await _pedidoRepository.ObterConexao()
+                .QueryAsync<PedidoDTO, PedidoItemDTO, EnderecoDTO, PedidoDTO>(sql, (p, pi, e) => 
+                {
+                    p.PedidoItems.Add(pi);
+                    p.Endereco = e;
+                    return p;
+                }, new { clienteId });
+
             return MapearPedido();
         }
 
