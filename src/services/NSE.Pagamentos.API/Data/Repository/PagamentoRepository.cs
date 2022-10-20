198719cb3d6e02599af1e43981c9e0e56a8f8ca9
Bryan <bryanlima200@outlook.com>
2022-10-20 00:31:52 -0300

    Implementado captura e cancelamento de pagamentos

--- src/services/NSE.Pagamentos.API/Data/Repository/PagamentoRepository.cs
+++ src/services/NSE.Pagamentos.API/Data/Repository/PagamentoRepository.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Core.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Core.Data;
 using NSE.Pagamentos.API.Models;
 
 namespace NSE.Pagamentos.API.Data.Repository
@@ -17,6 +18,21 @@ namespace NSE.Pagamentos.API.Data.Repository
         public void AdicionarPagamento(Pagamento pagamento)
         {
             _context.Pagamentos.Add(pagamento);
+        }
+
+        public void AdicionarTransacao(Transacao transacao)
+        {
+            _context.Transacoes.Add(transacao);
+        }
+
+        public async Task<Pagamento> ObterPagamentoPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Pagamentos.AsNoTracking().FirstOrDefaultAsync(p => p.PedidoId == pedidoId);
+        }
+
+        public async Task<IEnumerable<Transacao>> ObterTransacaoesPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Transacoes.AsNoTracking().Where(t => t.Pagamento.PedidoId == pedidoId).ToListAsync();
         }
 
         public void Dispose()
--- src/services/NSE.Pagamentos.API/Services/PagamentoIntegrationHandler.cs
+++ src/services/NSE.Pagamentos.API/Services/PagamentoIntegrationHandler.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Core.Messages.Integration;
+﻿using NSE.Core.DomainObjects;
+using NSE.Core.Messages.Integration;
 using NSE.MessageBus;
 using NSE.Pagamentos.API.Models;
 
@@ -18,12 +19,20 @@ namespace NSE.Pagamentos.API.Services
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             SetResponder();
+            SetSubscribers();
             return Task.CompletedTask;
         }
 
         private void SetResponder()
         {
             _bus.RespondAsync<PedidoIniciadoIntegrationEvent, ResponseMessage>(async request => await AutorizarPagamento(request));
+        }
+
+        private void SetSubscribers()
+        {
+            _bus.SubscribeAsync<PedidoCanceladoIntegrationEvent>("PedidoCancelado", async request => await CancelarPagamento(request));
+
+            _bus.SubscribeAsync<PedidoBaixadoEstoqueIntegrationEvent>("PedidoBaixadoEstoque", async request => await CapturarPagamento(request));
         }
 
         private async Task<ResponseMessage> AutorizarPagamento(PedidoIniciadoIntegrationEvent message)
@@ -46,6 +55,34 @@ namespace NSE.Pagamentos.API.Services
             }
 
             return response;
+        }
+
+        private async Task CancelarPagamento(PedidoCanceladoIntegrationEvent message)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var pagamentoService = scope.ServiceProvider.GetRequiredService<IPagamentoService>();
+
+                var response = await pagamentoService.CancelarPagamento(message.PedidoId);
+
+                if (!response.ValidationResult.IsValid)
+                    throw new DomainException($"Falha ao cancelar pagamento do pedido {message.PedidoId}");
+            }
+        }
+
+        private async Task CapturarPagamento(PedidoBaixadoEstoqueIntegrationEvent message)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var pagamentoService = scope.ServiceProvider.GetRequiredService<IPagamentoService>();
+
+                var response = await pagamentoService.CapturarPagamento(message.PedidoId);
+
+                if (!response.ValidationResult.IsValid)
+                    throw new DomainException($"Falha ao capturar pagamento do pedido {message.PedidoId}");
+
+                await _bus.PublishAsync(new PedidoPagoIntegrationEvent(message.ClienteId, message.PedidoId));
+            }
         }
     }
 }
